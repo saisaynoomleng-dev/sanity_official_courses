4e2b306d8661194577cb98f9b17bc939c4bb9a3e
sai_say_noom_leng-dev <snoomleng1@cougars.ccis.edu>
2025-03-27 07:56:05 -0500

    finished studio excellence

--- studio_excellence/sanity/structure.ts
+++ studio_excellence/sanity/structure.ts
@@ -1,4 +1,6 @@
-import { BiCalendar } from "react-icons/bi";
+import { BiAlbum, BiCalendar } from "react-icons/bi";
+import { FaCalendar, FaUser } from "react-icons/fa";
+import { FaLocationPin } from "react-icons/fa6";
 import { StructureResolver } from "sanity/structure";
 
 export const structure: StructureResolver = (S) => 
@@ -6,10 +8,21 @@ export const structure: StructureResolver = (S) =>
         .id('root')
         .title('Contents')
         .items([
+            S.documentTypeListItem('event').title('All Events').icon(BiCalendar),
+            S.divider(),
             S.listItem()
                 .title('Upcoming Events')
                 .icon(BiCalendar)
                 .schemaType('event')
                 .child(S.documentList().title('Upcoming Events').filter('dateAndTime >= now()')),
-            
+            S.listItem()
+                .title('Previous Events')
+                .icon(BiCalendar)
+                .schemaType('event')
+                .child(S.documentList().title('Previous Events').filter('dateAndTime <= now()')),
+            S.divider(),
+            S.documentTypeListItem('artist').title('All Artists').icon(FaUser),
+            S.documentTypeListItem('album').title('All Albums').icon(BiAlbum),
+            S.divider(),
+            S.documentTypeListItem('venue').title('All Venues').icon(FaLocationPin),
         ])
